6fd52e4f12b59e6c8187096e2164cdf54d227f77
Amna Jehanzeb <164073621+j27amna@users.noreply.github.com>
2025-01-30 23:57:34 +0500

    Add files via upload

---
+++ src/app/components/Journey.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Journey = () => {
+  return (
+    <main className='w-full h-full justify-center items-center'>
+        <video src="/video2.mp4" autoPlay loop muted className='w-full h-screen object-cover relative brightness-75'/>
+        <div className='w-full h-full flex-col  text-[#FFFFFF] absolute text-center mt-[-600px]'>
+            <h1 className='w-full h-full text-[40px] font-bold'>My Journey as a Frontend Developer</h1>
+            <p className='text-clip mt-[-580px]'>My journey into frontend development started with curiosity and a deep desire <br /> to build beautiful, functional interfaces. As I navigated through the fundamentals <br /> of HTML and CSS, I discovered the magic of bringing 
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            designs to life. But I didn’t <br /> stop there—I ventured into TypeScript, strengthening my understanding of JavaScript <br /> while writing cleaner, more maintainable code.
+
+My learning path took a structured <br /> turn when I joined GIAIC, where I built a strong foundation in Next.js and Tailwind CSS. <br /> These technologies became my creative tools, allowing me to craft pixel-perfect <br /> designs with precision. One of my proudest moments was developing the frontend <br /> for a Marketplace Builder Hackathon project, where I transformed a Figma design into <br/> a fully functional website in 24 hours.
+
+<br /> <br />
+
+Balancing my college studies with multiple courses was challenging, but I embraced <br /> the process, pushing through each hurdle with determination. Along the way, <br /> I also discovered the power of Generative AI and LLMs, adding another <br /> layer to my tech perspective.
+{/* eslint-disable-next-line react/no-unescaped-entities */}
+Now, as I continue refining my skills, <br />  I’m excited to share my journey and insights on LinkedIn, <br />  connecting with like-minded individuals. With every <br />  project, I see growth—not just in my coding ability <br /> but in my vision for creating seamless <br />  digital experiences.</p>
+            
+        </div>
+
+    </main>
+  )
+}
+
+export default Journey
